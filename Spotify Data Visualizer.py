5151fe43d555512bc95baf79dbaea44a82d60cbc
Michael Ching <michaeljune101@yahoo.com>
2022-05-06 17:31:39 -0700

    Added functions

--- Spotify Data Visualizer.py
+++ Spotify Data Visualizer.py
@@ -5,58 +5,113 @@ import matplotlib.pyplot as plt
 import json
 from collections import Counter
 
+#songHistory is a dictonary of song(key)-timesPlayed(value), amountOfFile is user input int
+def readSongHistory(songHistory, amountOfFiles):
 
-f = open('StreamingHistory0.json')
-data = json.load(f)
-songHistory = dict()
+    #Read X number of files
+    for i in range(amountOfFiles):
+        try:
+            fileName = ('StreamingHistory' + str(i) + '.json')
+            #print (fileName)
+            f = open(fileName)
+            data = json.load(f)
+        except:
+            print("Cannot read file")
 
-#Get each music stream
-for song in data:
+        #Get each music stream
+        for song in data:
 
-    #If song already added to songHistory Dictionary, update times played
-    if(song['trackName'] in songHistory):
-        timesPlayed = songHistory[song['trackName']]
-        timesPlayed = timesPlayed + 1
-        songHistory[song['trackName']] = timesPlayed
+            #If song already added to songHistory Dictionary, update times played
+            if(song['trackName'] in songHistory):
+                timesPlayed = songHistory[song['trackName']]
+                timesPlayed = timesPlayed + 1
+                songHistory[song['trackName']] = timesPlayed
 
-    #Song doesn't exist in songHistory dictionary, initialize
-    else:
-        songHistory[song['trackName']] = 1
-
-    
-#print (songHistory)
-print ('Total unique songs listened to: ', len(songHistory))
-
-plt.figure()
-
-sortSongHistory = {}
-sortByMostPlayed = sorted(songHistory, key=songHistory.get, reverse=True) 
-
-for w in sortByMostPlayed:
-    sortSongHistory[w] = songHistory[w]
-
-print (sortSongHistory)
-
-#For each song ever played
-#for song in sortSongHistory:
-    #print(song)
- #   plt.bar(song, sortSongHistory.get(song) )
+            #Song doesn't exist in songHistory dictionary, initialize
+            else:
+                songHistory[song['trackName']] = 1
 
 
-#Top 10 most listened songs, using iterator
-it = iter(sortSongHistory)
-for song in range(10):
 
-    #Get song title
-    songTitle = next(it)
+def main():
+     print("hello world")
+     songHistory = dict()
 
-    #X-cords is song title, Y-cords is the value/number of times played
-    plt.bar(songTitle, sortSongHistory.get(songTitle) )
+     numOfFiles = int(input("How many files to read?") )
 
-    #Show number of times played on graph
-    plt.text(song, sortSongHistory.get(songTitle), sortSongHistory.get(songTitle) ,color = 'blue', fontweight = 'bold')
+    #Read all songs from files
+     readSongHistory(songHistory, numOfFiles)
 
-    #Iterate to next song
-    next(it)
+     #print (songHistory)
+     print ('Total unique songs listened to: ', len(songHistory))
 
-plt.show()
+     topXSongs = int(input("Range of top songs?") )
+     topSongsListened(songHistory, topXSongs)
+
+
+
+#Sort songHistory dict by most played
+def sortByMostPlayed(songHistory):
+    sortSongHistory = {}
+    sortByMostPlayed = sorted(songHistory, key=songHistory.get, reverse=True) 
+
+    for w in sortByMostPlayed:
+        sortSongHistory[w] = songHistory[w]
+
+    print (sortSongHistory)
+
+    return sortSongHistory
+
+
+def allSongsListened():
+    print()
+    #For each song ever played
+    #for song in sortSongHistory:
+        #print(song)
+    #   plt.bar(song, sortSongHistory.get(song) )
+
+
+
+#Plot the top 'number' of songs listened to
+#songHistory is dict of all songs, number is user input of desired X top songs
+def topSongsListened(songHistory, number):
+    sortSongHistory = sortByMostPlayed(songHistory)
+
+    plt.figure()
+
+    #Top number most listened songs, using iterator
+    it = iter(sortSongHistory)
+    for song in range(number):
+
+        #Get song title
+        songTitle = next(it)
+
+        #X-cords is song title, Y-cords is the value/number of times played
+        plt.bar(songTitle, sortSongHistory.get(songTitle) )
+
+        #Show number of times played on graph
+        plt.text(song, sortSongHistory.get(songTitle), sortSongHistory.get(songTitle) ,color = 'blue', fontweight = 'bold')
+
+        #Iterate to next song
+        next(it)
+
+    plt.show()
+
+
+
+def main():
+     print("hello world")
+     songHistory = dict()
+
+     numOfFiles = int(input("How many files to read?") )
+
+    #Read all songs from files
+     readSongHistory(songHistory, numOfFiles)
+
+     #print (songHistory)
+     print ('Total unique songs listened to: ', len(songHistory))
+
+     topXSongs = int(input("Range of top songs?") )
+     topSongsListened(songHistory, topXSongs)
+
+main()
